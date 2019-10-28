31644629fc31a27039cafb5f3c1d8a1003087ca3
danielbellini <54839669+danielbellini@users.noreply.github.com>
2019-10-28 12:17:27 +0100

    Update Queries 2 and 5

--- Consultas_USFlights.sql
+++ Consultas_USFlights.sql
@@ -1,19 +1,19 @@
+/*ejercicio 1*/
 SELECT count(*) from Flights
 
+/*ejercicio 2*/
 SELECT Origin, avg(ArrDelay) as prom_aribades, avg(DepDelay) as prom_sortides
-FROM `usairports` 
-LEFT JOIN `flights` 
-ON `usairports`.IATA =`flights`.Origin
+FROM `flights` 
 GROUP BY Origin
 ORDER by Origin ASC
 
-
+/*ejercicio 3*/
 SELECT Origin, colYear, colMonth, avg(ArrDelay) as prom_aribades
 FROM `flights`
 GROUP by Origin, colYear, colMonth
 ORDER by Origin ASC
 
-
+/*ejercicio 4*/
 SELECT City, colYear, colMonth, avg(ArrDelay) as prom_aribades
 FROM `flights`
 LEFT JOIN `usairports` 
@@ -21,11 +21,13 @@ ON `flights`.Origin = `usairports`.IATA
 GROUP by City, colYear, colMonth
 ORDER by City ASC
 
+/*ejercicio 5*/
 SELECT UniqueCarrier, colYear, colMonth, avg(ArrDelay), sum(Cancelled) as total_cancelled
 FROM `flights`
-Group BY  colYear, colMonth,Cancelled
+Group BY  colYear, colMonth, Cancelled
 ORDER by total_cancelled DESC, colYear DESC, colMonth DESC
 
+/*ejercicio 6*/
 SELECT TailNum, sum(Distance) as total_distance
 FROM Flights 
 WHERE TailNum <> '' AND TailNum <> '#NOME?'
@@ -33,6 +35,7 @@ GROUP BY TailNum
 ORDER BY total_distance DESC
 LIMIT 10
 
+/*ejercicio 7*/
 SELECT UniqueCarrier, avg(ArrDelay) as avg_delay
 FROM Flights 
 GROUP BY UniqueCarrier
